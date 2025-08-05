4cb34870c7e707097007e064a051066476b52711
romeljhon salvaleon <pajojhon@gmail.com>
2025-08-05 11:34:55 +0000

    make the color of the meta logo into primary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Space_Grotesk } from 'next/font/google';
 
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], variable: '--font-space-grotesk' });
 
-const logoSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 10.625l5-3.125 5 3.125-5 3.125-5-3.125z"></path><path d="M7 14.375l5-3.125 5 3.125-5 3.125-5-3.125z"></path><path d="M7 18.125l5-3.125 5 3.125-5 3.125-5-3.125z"></path></svg>`;
+const logoSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="hsl(217.2 91.2% 59.8%)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 10.625l5-3.125 5 3.125-5 3.125-5-3.125z"></path><path d="M7 14.375l5-3.125 5 3.125-5 3.125-5-3.125z"></path><path d="M7 18.125l5-3.125 5 3.125-5 3.125-5-3.125z"></path></svg>`;
 const logoDataUri = `data:image/svg+xml;base64,${Buffer.from(logoSvg).toString('base64')}`;
 
 export const metadata: Metadata = {
